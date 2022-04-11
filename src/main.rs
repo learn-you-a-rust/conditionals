b8b1d48d8da4926d72cdfc854332c6a5b4082654
greyspectrum <hello@greyspectrum.io>
2022-04-11 13:38:26 -0400

    to the lighthouse

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 fn main() {
-    // if is an expression, so it can be used
-    // on the right side of a 'let' statement
-    
-    let condition = true;
-    let number = if condition {
-        5
-    } else { 
-        6 // note the lack of semicolon
-          // on this expression
+// To the Lighthouse
+    let fine = false;
+    let weather = if fine {
+        1
+    } else {
+        3650
     };
 
-    println!("The value of number is: {}", number);
+    println!("We will go to the Lighthouse in {} days", weather);
 }
